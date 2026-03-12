d64fc2d4838143114a4c772cb7afdc0d600a1cd1
ADRIAN RODRIGUEZ <202040994@uthh.edu.mx>
2026-03-12 09:49:17 -0600

    so

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import equipoRoutes    from './src/routes/equipos.routes.js'
 import categoriaRoutes from './src/routes/categorias.routes.js'
-// import solicitudRoutes from './src/routes/solicitudes.routes.js'
+import solicitudRoutes from './src/routes/solicitudes.routes.js'
 
 module.exports = app;
 
@@ -17,7 +17,7 @@ app.use(express.json())
 // Rutas
 app.use('/api/equipos',    equipoRoutes)
 app.use('/api/categorias', categoriaRoutes)
-// app.use('/api/solicitudes', solicitudRoutes)
+app.use('/api/solicitudes', solicitudRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
